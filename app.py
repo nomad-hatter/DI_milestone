25a797e655293d21378526e85a677ab8f0338e30
Usman Raza <usman.rz@mac.com>
2015-12-14 13:55:50 -0500

    update

--- app.py
+++ app.py
@@ -9,14 +9,15 @@ def main():
 @app.route('/index',methods=['GET','POST'])
 def index():
     
-    
-    if request.method == 'POST':
-        app.vars['ticker']=request.form['ticker']
-         f = open('%s.txt'%(app.vars['ticker']),'w')
-         f.write('%s\n'%(app.vars['ticker']))
-         f.close()
+    if request.method == 'GET':
+          return render_template('index.html')
     else:
-  return render_template('index.html')
+        app.vars['ticker']=request.form['ticker']
+        f = open('%s.txt'%(app.vars['ticker']),'w')
+        f.write('%s\n'%(app.vars['ticker']))
+        f.close()
+        
+        return 'request.method was not GET!'
 
 if __name__ == '__main__':
   app.run(port=33507)
