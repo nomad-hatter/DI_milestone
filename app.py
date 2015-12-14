b0cb8d651c05fbd6c181fec38d79bf13dea11a76
Usman Raza <usman.rz@mac.com>
2015-12-14 14:29:52 -0500

    update

--- app.py
+++ app.py
@@ -15,29 +15,29 @@ def index():
     else:
         app.vars['ticker']=request.form['ticker']
         
-        api_url='https://www.quandl.com/api/v3/datasets/WIKI/%s.json?api_key=tX-ANP6Rh24Q81bFsYH5'%(app.vars['ticker'])
+        #api_url='https://www.quandl.com/api/v3/datasets/WIKI/%s.json?api_key=tX-ANP6Rh24Q81bFsYH5'%(app.vars['ticker'])
 
-        df = pd.read_json(api_url)
-        df = pd.DataFrame(df['dataset']['data'])
+        #df = pd.read_json(api_url)
+        #df = pd.DataFrame(df['dataset']['data'])
         
         # set up some data
-        x=pd.to_datetime(pd.Series(df[0]))
-        x=x.tolist()
-        y = df[1].tolist()
+        #x=pd.to_datetime(pd.Series(df[0]))
+        #x=x.tolist()
+        #y = df[1].tolist()
         
-        output_notebook()
+        #output_notebook()
         
         # create a new plot with figure
-        p = figure(plot_width=800, plot_height=800, x_axis_type='datetime',title='Ticker Data',x_axis_label='date', y_axis_label='price')
+        #p = figure(plot_width=800, plot_height=800, x_axis_type='datetime',title='Ticker Data',x_axis_label='date', y_axis_label='price')
 
         # add both a line and circles on the same plot
-        p.line(x,y, line_width=2)
+        #p.line(x,y, line_width=2)
 
-        output_file("graph.html")
-        show(p) # show the results
-        from bokeh.embed import components 
+        #output_file("graph.html")
+        #show(p) # show the results
+        #from bokeh.embed import components 
 
-        script, div = components(p)
+        #script, div = components(p)
         return '%s'%(app.vars['ticker'])
         #return render_template('graph.html', script=script, div=div)
         #return render_template('index.html')
