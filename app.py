fa68f0466944b2ccf00e2d3571d7af5fe1a6fabc
Usman Raza <usman.rz@mac.com>
2015-12-14 14:58:20 -0500

    update

--- app.py
+++ app.py
@@ -14,10 +14,10 @@ def index():
           return render_template('index.html')
 
     else:
-    #from bokeh.io import output_notebook, show
+    from bokeh.io import output_notebook, show
     #from bokeh.plotting import figure
     #import requests
-    import pandas as pd
+    #import pandas as pd
 
         app.vars['ticker']=request.form['ticker']
         
