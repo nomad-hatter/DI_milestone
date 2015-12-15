5683540ee5947ac1e8696478c2078605d08ec2fb
Usman Raza <usman.rz@mac.com>
2015-12-15 16:37:36 -0500

    update

--- app.py
+++ app.py
@@ -31,7 +31,7 @@ def graph():
         crnt_date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
         start_date = time.strftime('%Y-%m-%d',time.localtime(time.time()-60*60*24*31))
 
-        api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + app.vars['ticker'] + '.json?start_date=' + start_date + '&end_date=' + crnt_date + '?api_key=tX-ANP6Rh24Q81bFsYH5'
+        api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/' + app.vars['ticker']# + '.json?start_date=' + start_date + '&end_date=' + crnt_date + '?api_key=tX-ANP6Rh24Q81bFsYH5'
 
         #app.vars['api_url']=api_url
         #df = pd.read_json(api_url)
