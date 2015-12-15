bdbf92f21e2d4ab5d263c38d0949518ef2a522b5
Usman Raza <usman.rz@mac.com>
2015-12-15 15:54:40 -0500

    update

--- app.py
+++ app.py
@@ -27,30 +27,31 @@ def index():
 @app.route('/graph',methods=['GET','POST'])
 def graph():
     
-        crnt_date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
-        start_date = time.strftime('%Y-%m-%d',time.localtime(time.time()-60*60*24*31))
+        #crnt_date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
+        #start_date = time.strftime('%Y-%m-%d',time.localtime(time.time()-60*60*24*31))
 
         
-        api_url='https://www.quandl.com/api/v3/datasets/WIKI/%s.json?start_date=%s&end_date=%s?api_key=tX-ANP6Rh24Q81bFsYH5'%(app.vars['ticker'],start_date,crnt_date)
+        #api_url='https://www.quandl.com/api/v3/datasets/WIKI/%s.json?start_date=%s&end_date=%s?api_key=tX-ANP6Rh24Q81bFsYH5'%(app.vars['ticker'],start_date,crnt_date)
 
-        df = pd.read_json(%s)%(api_url)
-        df = pd.DataFrame(df['dataset']['data'])
+        #df = pd.read_json(%s)%(api_url)
+        #df = pd.DataFrame(df['dataset']['data'])
         
         # set up some data
-        x=pd.to_datetime(pd.Series(df[0]))
-        x=x.tolist()
-        y = df[1].tolist()
+        #x=pd.to_datetime(pd.Series(df[0]))
+        #x=x.tolist()
+        #y = df[1].tolist()
           
         # create a new plot with figure
-        p = figure(plot_width=800, plot_height=800, x_axis_type='datetime',title='Ticker Data',x_axis_label='date', y_axis_label='price')
+        #p = figure(plot_width=800, plot_height=800, x_axis_type='datetime',title='Ticker Data',x_axis_label='date', y_axis_label='price')
 
-        p.line(x,y, line_width=2)
+        #p.line(x,y, line_width=2)
 
         #resources = RESOURCES.render(js_raw=INLINE.js_raw,css_raw=INLINE.css_raw,js_files=INLINE.js_files,css_files=INLINE.css_files,)
 
-        script, div = components(p)
+        #script, div = components(p)
 
-        return render_template('graph.html',ticker = app.vars['ticker'],script=script, div=div)   
+        #return render_template('graph.html',ticker = app.vars['ticker'],script=script, div=div) 
+        return 'redirect works'
     
 
 if __name__ == '__main__':
