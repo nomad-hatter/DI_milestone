fc0c747b9cff4e989b32b0d190c4e01c895c9baf
Usman Raza <usman.rz@mac.com>
2015-12-16 13:23:05 -0500

    update

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
 from bokeh.io import output_notebook, show
 from pandas import DataFrame
 from bokeh.plotting import figure
