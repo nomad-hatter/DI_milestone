c43e2147dbba6dbff2b13ba3cbe14bd805f265e7
Usman Raza <usman.rz@mac.com>
2015-12-16 14:06:54 -0500

    update

--- app.py
+++ app.py
@@ -38,7 +38,7 @@ def graph():
 
         try:
             df = pd.read_json(api_url)
-        except:
+        except Exception:
             flash('test')
             return redirect(url_for('index'))
         
