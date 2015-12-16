edb68d2883a352d32727b55b0c3c5a65834acd1c
Usman Raza <usman.rz@mac.com>
2015-12-16 10:21:59 -0500

    update

--- app.py
+++ app.py
@@ -24,12 +24,15 @@ def index():
         #app.vars['ticker']=request.form['ticker']
         symbol=request.form['ticker']
         #session['symbol']=request.form['ticker']
-        return redirect('/graph', symbol='AAPL')
+        return redirect('/graph?symbol='+symbol)
+
+
 @app.route('/graph',methods=['GET','POST'])
 def graph(symbol):
         crnt_date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
         start_date = time.strftime('%Y-%m-%d',time.localtime(time.time()-60*60*24*31))
         
+        #symbol=request.args.get
         #symbol=request.args['symbol']
         #api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/'+ticker+'.json?start_date=' + start_date + '&end_date=' + crnt_date + '?api_key=tX-ANP6Rh24Q81bFsYH5l'
     
