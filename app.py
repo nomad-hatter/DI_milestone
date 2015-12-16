86c4e68cb13f1d9a5de75ec085ae715c4a58dc73
Usman Raza <usman.rz@mac.com>
2015-12-16 14:33:55 -0500

    update

--- app.py
+++ app.py
@@ -12,46 +12,46 @@ app.var={}
 
 @app.route('/')
 def main():
-  return redirect('/index')
+    return redirect('/index')
 
 @app.route('/index',methods=['GET','POST'])
 def index():
 
     if request.method == 'GET':
-          return render_template('index.html')
+        return render_template('index.html')
 
     else:
         symbol=request.form['ticker']
-        api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/'+symbol+'.json?api_key=tX-ANP6Rh24Q81bFsYH5l'
-        try:
-            df = pd.read_json(api_url)
-        except Exception as e:
-            flash('Invalid ticker symbol')
-            return render_template("index.html")
+        #api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/'+symbol+'.json?api_key=tX-ANP6Rh24Q81bFsYH5l'
+        #try:
+        #df = pd.read_json(api_url)
+        #except Exception as e:
+        flash('Invalid ticker symbol')
+        #return render_template("index.html")
         return redirect(url_for('graph'))
 
-    
-@app.route('/graph',methods=['GET','POST'])
-def graph():
+
+    @app.route('/graph',methods=['GET','POST'])
+    def graph():
         crnt_date = time.strftime('%Y-%m-%d',time.localtime(time.time()))
         start_date = time.strftime('%Y-%m-%d',time.localtime(time.time()-60*60*24*28))
-        
+
         symbol=request.form['ticker']
         option=request.form['option']
-        
+
         symbol=symbol.upper()
-        
+
         api_url = 'https://www.quandl.com/api/v3/datasets/WIKI/'+symbol+'.json?start_date=' + start_date + '&end_date=' + crnt_date + '&column_index='+ option +'?api_key=tX-ANP6Rh24Q81bFsYH5l'
-                    df = pd.read_json(api_url)
+        df = pd.read_json(api_url)
 
         plotTitle=symbol+' '+df['dataset']['column_names'][1]+' Data'
         df = pd.DataFrame(df['dataset']['data'])
-        
+
         # set up some data
         x=pd.to_datetime(pd.Series(df[0]))
         x=x.tolist()
         y = df[1].tolist()
-        
+
         # create a new plot with figure
         p = figure(plot_width=500, plot_height=500, x_axis_type='datetime',title=plotTitle,x_axis_label='Date', y_axis_label='Price')
 
@@ -60,6 +60,6 @@ def graph():
         script, div = components(p)
 
         return render_template('graph.html',script=script, div=div, symbol=symbol) 
-        
-if __name__ == '__main__':
-    app.run(port=33507)
+
+    if __name__ == '__main__':
+        app.run(port=33507)
