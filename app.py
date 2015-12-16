39fc22358a4d37b56d21d050cc325973a0548df3
Usman Raza <usman.rz@mac.com>
2015-12-16 13:39:25 -0500

    update

--- app.py
+++ app.py
@@ -39,6 +39,7 @@ def graph():
         try:
             df = pd.read_json(api_url)
         except:
+            flash('test')
             return redirect(url_for('index'))
         
         plotTitle=symbol+' '+df['dataset']['column_names'][1]+' Data'
